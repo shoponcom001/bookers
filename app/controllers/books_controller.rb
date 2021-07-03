1bc606105b1b759df3cd7f4bd81f3dc8a263ed6d
shoponcom001 <shopon.com001@gmail.com>
2021-07-03 13:34:08 +0000

    push

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = book.find(params[])
+  end
+
+  def new
+    @book = book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
+  end
+
+end
