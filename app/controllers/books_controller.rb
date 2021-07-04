bfbbdacfb5a55cb1bde33e7f628bb0ea8c4d0877
shoponcom001 <shopon.com001@gmail.com>
2021-07-04 02:17:02 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,9 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: "Book was successfully created."
+    end
   end
 
   def edit
@@ -23,8 +24,9 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book), notice: "Book was successfully updated."
+    end
   end
 
   def destroy
